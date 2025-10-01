3f6219bfc9572b860e559ddeb1051610b0803cf3
Jacob Ketola <h65k935@msu.montana.edu>
2025-10-01 12:34:05 -0600

    homework5

--- phpformviewer-homework5/form-viewer.php
+++ phpformviewer-homework5/form-viewer.php
@@ -6,6 +6,11 @@ $data = $_REQUEST;
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style>
+    table { border-collapse: collapse; width: 100%; margin-top: 20px; background: white; }
+    th, td { border: 1px solid #333; padding: 10px; }
+    th { background: #ddd; }
+  </style>
     <title>Form Data Viewer</title>
 </head>
 <body>
